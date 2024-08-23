be0f3e99fb048479aa3168937822bc7a2de203ce
David Grammatico <david.lugdunum@gmail.com>
2024-08-23 19:23:11 +0200

    update style

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -4,7 +4,7 @@ import { Text, View } from 'react-native';
 export default function HomeScreen() {
 	return (
 		<View className=" bg-slate-600 flex-1 items-center justify-center">
-			<Text className="text-2xl text-blue-500 font-bold">Feed</Text>
+			<Text className=" text-xs md:text-4xl text-blue-500 font-bold">Feed</Text>
 		</View>
 	);
 }
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -4,3 +4,5 @@ import '../../global.css';
 export default function RootLayout() {
 	return <Stack screenOptions={{ headerShown: false }} />;
 }
+
+// 1:02:00 => youtube
