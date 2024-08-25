e90da1461b018ea6cbc64bdf70e002713bb80764
David Grammatico <david.lugdunum@gmail.com>
2024-08-25 20:29:58 +0200

    chore: Update userInterfaceStyle to automatic in app.json and add posts.json to assets/data

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
 import { Text, View } from 'react-native';
+import posts from '@assets/data/posts.json';
+
+const post1 = posts[0];
 
 export default function HomeScreen() {
+	console.log(post1);
 	return (
 		<View className=" bg-slate-600 flex-1 items-center justify-center">
 			<Text className=" text-xs md:text-4xl text-blue-500 font-bold">Feed</Text>
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,5 @@
-import { View, Text } from 'react-native';
 import React from 'react';
-import { Link, Redirect } from 'expo-router';
+import { Redirect } from 'expo-router';
 
 export default function HomeScreen() {
 	return <Redirect href={'/(tabs)'} />;
