0d0999109e0e724c6aff04046923dc52c91f193c
David Grammatico <david.lugdunum@gmail.com>
2024-09-03 18:26:17 +0200

    update style for feed screen

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,13 +1,22 @@
-import { Text, View } from 'react-native';
+import { View, Image } from 'react-native';
 import posts from '@assets/data/posts.json';
 
 const post1 = posts[0];
 
-export default function HomeScreen() {
+export default function FeedScreen() {
 	console.log(post1);
 	return (
-		<View className=" bg-slate-600 flex-1 items-center justify-center">
-			<Text className=" text-xs md:text-4xl text-blue-500 font-bold">Feed</Text>
+		<View className="">
+			<View className="p-2">
+				<Image
+					source={{ uri: post1.user.image_url }}
+					className="w-12 aspect-square rounded-full"
+				/>
+			</View>
+			<Image
+				source={{ uri: post1.image_url }}
+				className="w-full aspect-[4/3]"
+			/>
 		</View>
 	);
 }
